f250f97e4d65602f639f740fe3c6dc61ce7fdd5f
EMIC Compiler <emic-compiler@emic.io>
2026-06-28 12:16:13 -0300

    Code generation: 2026-06-28 15:16:13 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: boot
    Project: boot-build
    User: mariano-at-hunkeler-com-ar

--- inc/boot_id.h
+++ inc/boot_id.h
@@ -1,7 +1,7 @@
 #ifndef BOOT_ID_H
 #define BOOT_ID_H
 
-#define BOOT_MODULE_ID      { 'Q','o','e','o','A','f' }
+#define BOOT_MODULE_ID      { 'H','B','F','0','D','y' }
 #define BOOT_MODULE_ID_LEN  6
 
 #endif /* BOOT_ID_H */
